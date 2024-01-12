c4d0dd5f5e6dc1849989e81ace78e33138a28878
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2024-01-12 08:56:39 +0200

    Add middlewares

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,6 +1,17 @@
 const express = require('express')
 const app = express()
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
+
 let persons = [
   {
     name: 'Arto Hellas',
@@ -32,8 +43,6 @@ const infoPageTemplate = (time) => `
 
 const generateId = (min, max) =>
   Math.floor(Math.random() * (max - min + 1)) + min
-
-app.use(express.json())
 
 app.get('/info', (request, response) => {
   response.send(infoPageTemplate(new Date()))
@@ -90,6 +99,12 @@ app.delete('/api/persons/:id', (request, response) => {
 
   response.status(204).end()
 })
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
 
 const PORT = 3001
 app.listen(PORT, () => {
