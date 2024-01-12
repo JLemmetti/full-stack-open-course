e860fd3b13f5602aa1274dd86161ae47e9efe756
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2024-01-12 09:57:37 +0200

    Add logging with Morgan

--- osa3/puhelinluettelo/index.js
+++ osa3/puhelinluettelo/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -11,6 +12,14 @@ const requestLogger = (request, response, next) => {
 
 app.use(express.json())
 app.use(requestLogger)
+
+morgan.token('body', (req) => {
+  return JSON.stringify(req.body)
+})
+
+app.use(
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 let persons = [
   {
