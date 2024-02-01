a68621dae36918d4e274bb55614a8719ae039d31
Jurkka Lemmetti <jurkka.lemmetti@gmail.com>
2024-02-01 13:05:38 +0200

    Custom validator for phone numbers

--- osa3/puhelinluettelo/backend/models/person.js
+++ osa3/puhelinluettelo/backend/models/person.js
@@ -16,8 +16,21 @@ mongoose
   })
 
 const personSchema = new mongoose.Schema({
-  name: String,
-  number: String,
+  name: {
+    type: String,
+    minlength: 3,
+    required: true,
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: (value) => {
+        return /^\d{2,3}-\d{5,}$/.test(value) && value.length >= 8
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+    required: [true, 'User phone number required'],
+  },
 })
 
 personSchema.set('toJSON', {
